9284a0b55b39f1ae050255d37dceaf7aff5a0a63
Vladislav Savionok <savionak@gmail.com>
2017-03-22 17:20:05 +0300

    Initial Commit

---
+++ CompositeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompositeApp
+//
+//  Created by Admin on 3/22/17.
+//  Copyright © 2017 BSUIR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
